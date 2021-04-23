57d2c89190b0ae9387d7228edb2201bd752e5268
Vincent74230 <vincent.nowak@hotmail.fr>
2021-04-23 14:24:47 +0200

    IMPROVED search_result frontend, added reduced profiles

--- application/views.py
+++ application/views.py
@@ -9,8 +9,13 @@ def index(request):
 def search_results(request):
 	"""Returns user search results"""
 	category_choice = ''
+	service_choice = ''
 	if request.method == 'POST':
 		category_choice = request.POST.get('which_category')
+		service_choice = request.POST.get('which_service')
+	print (category_choice)
+	print (service_choice)
+
 	categories = Category.objects.all()
 	services_dict = {}
 	category_dict = {}
@@ -23,6 +28,25 @@ def search_results(request):
 			users_service += User.objects.filter(proposed_services__name=service.name).count()
 		category_dict[category.name] = users_service
 		services_dict[category.name] = single_service_dict
+	'''
+	users = []
+	all_users = User.objects.all()
+	single_user = {}
+	proposed_services = []
+	required_services = []
+	for user in all_users:
+		single_user["username"] = user.username
+		single_user["postcode"] = user.postcode
+		user_proposed_services = Services.objects.filter(proposed_services__pk=user.pk)
+		for proposed_service in user_proposed_services:
+			proposed_services.append(proposed_service.name)
+		single_user["proposed"] = proposed_services
+		user_required_services = Services.objects.filter(required_services__pk=user.pk)
+		for required_service in user_required_services:
+			required_services.append(required_service.name)
+		single_user["required"] = proposed_services
+
+	'''
 
 	context = {
 	'nb_users':len(User.objects.all()),
