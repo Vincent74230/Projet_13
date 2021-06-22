31dedf15b72e5e50ff5bd7bc09fc85b2907a2028
Vincent74230 <vincent.nowak@hotmail.fr>
2021-06-22 18:59:53 +0200

    ADDED new js file for rating template, working on this

---
+++ static/js/script_ratings.js
@@ -0,0 +1,116 @@
+/*!
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+// Customization by https://github.com/Vincent74230
+// Code for star rating system
+
+const one = document.getElementById("first");
+const two = document.getElementById('second');
+const three = document.getElementById('third');
+const four = document.getElementById('fourth');
+const five = document.getElementById('fifth');
+
+const arr = [one, two, three, four, five]
+
+const handleSelect = (selection) => {
+    switch(selection){
+        case'first': {
+            one.classList.add('checked')
+            two.classList.remove('checked')
+            three.classList.remove('checked')
+            four.classList.remove('checked')
+            five.classList.remove('checked')
+            return
+        }
+        case'second': {
+            one.classList.add('checked')
+            two.classList.add('checked')
+            three.classList.remove('checked')
+            four.classList.remove('checked')
+            five.classList.remove('checked')
+            return
+        }
+        case'third': {
+            one.classList.add('checked')
+            two.classList.add('checked')
+            three.classList.add('checked')
+            four.classList.remove('checked')
+            five.classList.remove('checked')
+            return
+        }
+        case'fourth': {
+            one.classList.add('checked')
+            two.classList.add('checked')
+            three.classList.add('checked')
+            four.classList.add('checked')
+            five.classList.remove('checked')
+            return
+        }
+        case'fifth': {
+            one.classList.add('checked')
+            two.classList.add('checked')
+            three.classList.add('checked')
+            four.classList.add('checked')
+            five.classList.add('checked')
+            return
+        }
+    }
+}
+
+arr.forEach(item=> item.addEventListener('mouseover', (event)=>{
+    handleSelect(event.target.id)
+}));
