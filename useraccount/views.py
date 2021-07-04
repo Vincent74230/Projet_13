45c245fc803c00f7d5fee0690e9f8d196ec6e0c4
Vincent74230 <vincent.nowak@hotmail.fr>
2021-07-04 09:30:36 +0200

    LINTED useraccount/views

--- useraccount/views.py
+++ useraccount/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Category, User, Services, Rating
-from .forms import (
-    CreateUserForm, 
-    ModifyUserProfile, 
-    ChangePassword)
+from .forms import CreateUserForm, ModifyUserProfile, ChangePassword
 from django.views.generic import View, UpdateView
 from application.departements_list import DEPARTEMENTS as departements
 from django.contrib import messages
@@ -23,27 +20,35 @@ from .score_extend import ScoreRegistration
 
 class Index(View):
     form_class = CreateUserForm
-    template_name = 'useraccount/index.html'
+    template_name = "useraccount/index.html"
 
     def get(self, request, *args, **kwargs):
         form = self.form_class()
-        return render(request, self.template_name, {
-            'form': form,})
+        return render(
+            request,
+            self.template_name,
+            {
+                "form": form,
+            },
+        )
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
 
             user = form.save(commit=False)
-            user.is_active = False # Deactivate account till it is confirmed
+            user.is_active = False  # Deactivate account till it is confirmed
             user.save()
             current_site = get_current_site(request)
-            msg_html = render_to_string('useraccount/account_activation_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
+            msg_html = render_to_string(
+                "useraccount/account_activation_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
             send_mail(
                 "Bonjour {}, c'est PeopleSkills".format(user.username),
                 "Ceci est un message de PeopleSkills",
@@ -51,11 +56,17 @@ class Index(View):
                 [user.email],
                 html_message=msg_html,
             )
-            messages.success(request, ('Activez votre compte en cliquant sur le lien envoyé dans votre boite mail'))
+            messages.success(
+                request,
+                (
+                    "Activez votre compte en cliquant sur le lien envoyé dans votre boite mail"
+                ),
+            )
 
-            return redirect(reverse('useraccount:login'))
+            return redirect(reverse("useraccount:login"))
 
-        return render(request, self.template_name, {'form': form})
+        return render(request, self.template_name, {"form": form})
+
 
 def login_page(request):
     """Login page, checks username and password"""
@@ -75,8 +86,8 @@ def login_page(request):
 
     return render(request, "useraccount/login.html", {})
 
-class ActivateAccount(View):
 
+class ActivateAccount(View):
     def get(self, request, uidb64, token, *args, **kwargs):
         try:
             uid = force_text(urlsafe_base64_decode(uidb64))
@@ -89,11 +100,15 @@ class ActivateAccount(View):
             user.email_confirmed = True
             user.save()
             login(request, user)
-            messages.success(request, ('Votre compte a été confirmé.'))
-            return redirect('/')
+            messages.success(request, ("Votre compte a été confirmé."))
+            return redirect("/")
         else:
-            messages.warning(request, ('Lien de confirmation invalide, peut-être a-t-il déjà été utilisé.'))
-            return redirect('/')
+            messages.warning(
+                request,
+                ("Lien de confirmation invalide, peut-être a-t-il déjà été utilisé."),
+            )
+            return redirect("/")
+
 
 @login_required
 def log_out(request):
@@ -108,46 +123,58 @@ def my_account(request):
     choice = request.GET.get("choice")
     form = ModifyUserProfile(instance=request.user)
 
-    if choice == 'mon_profil':
-        if request.method == 'POST':
+    if choice == "mon_profil":
+        if request.method == "POST":
             form = ModifyUserProfile(request.POST, instance=request.user)
             if form.is_valid():
                 form.save()
-                messages.success(request, ('Votre profil a bien été modifié'))
-                return render(request, 'useraccount/myaccount.html', {'choice':choice, 'form':form})
-        return render(request, 'useraccount/myaccount.html', {'choice':choice, 'form':form})
+                messages.success(request, ("Votre profil a bien été modifié"))
+                return render(
+                    request,
+                    "useraccount/myaccount.html",
+                    {"choice": choice, "form": form},
+                )
+        return render(
+            request, "useraccount/myaccount.html", {"choice": choice, "form": form}
+        )
 
-    if choice == 'mes_identifiants':
-        if request.method == 'POST':
+    if choice == "mes_identifiants":
+        if request.method == "POST":
             form = ChangePassword(data=request.POST, user=request.user)
             if form.is_valid():
                 form.save()
-                messages.success(request, ('Votre mot de passe a bien été modifié'))
-                return render(request, 'useraccount/myaccount.html', {'choice':choice, 'form':form})
+                messages.success(request, ("Votre mot de passe a bien été modifié"))
+                return render(
+                    request,
+                    "useraccount/myaccount.html",
+                    {"choice": choice, "form": form},
+                )
         form = ChangePassword(user=request.user)
-        return render(request, 'useraccount/myaccount.html', {'choice':choice, 'form':form})
-    if choice == 'mes_services':
+        return render(
+            request, "useraccount/myaccount.html", {"choice": choice, "form": form}
+        )
+    if choice == "mes_services":
         user = User.objects.get(username=request.user.username)
 
-        #Making simple list of all services
+        # Making simple list of all services
         services_list = []
         for category, services in services_dict.items():
             for service in services:
                 services_list.append(service)
 
-        #Making list of all users required services
+        # Making list of all users required services
         user_required_services_name = []
         for element in user.required_services.all():
             user_required_services_name.append(element.name)
 
-        #Making list of all proposed services
+        # Making list of all proposed services
         user_proposed_services_name = []
         for element in user.proposed_services.all():
             user_proposed_services_name.append(element.name)
 
-        if request.method == 'POST':
-            #Grabing required services checked by user
-            checkboxes_required_services = request.POST.getlist('required')
+        if request.method == "POST":
+            # Grabing required services checked by user
+            checkboxes_required_services = request.POST.getlist("required")
             if checkboxes_required_services:
                 for service in services_list:
                     if service in checkboxes_required_services:
@@ -165,8 +192,8 @@ def my_account(request):
                 else:
                     pass
 
-            #Grabing proposed services checked by user from frontend
-            checkboxes_proposed_services = request.POST.getlist('proposed')
+            # Grabing proposed services checked by user from frontend
+            checkboxes_proposed_services = request.POST.getlist("proposed")
             if checkboxes_proposed_services:
                 for service in services_list:
                     if service in checkboxes_proposed_services:
@@ -193,17 +220,17 @@ def my_account(request):
                 user_proposed_services_name.append(element.name)
 
         context = {
-            'choice':choice,
-            'user_required_services_name':user_required_services_name,
-            'user_proposed_services_name':user_proposed_services_name,
-            'services_dict':services_dict,
-            }
-        return render(request, 'useraccount/myaccount.html', context)
-    if choice == 'mes_avis':
-        #Fetching score received by current user
+            "choice": choice,
+            "user_required_services_name": user_required_services_name,
+            "user_proposed_services_name": user_proposed_services_name,
+            "services_dict": services_dict,
+        }
+        return render(request, "useraccount/myaccount.html", context)
+    if choice == "mes_avis":
+        # Fetching score received by current user
         score_received = Rating.objects.filter(receiver_id=request.user.pk)
 
-        #Fetching score sent by current user
+        # Fetching score sent by current user
         score_sent = Rating.objects.filter(sender_id=request.user.pk)
 
         scores_dict_list = []
@@ -211,74 +238,124 @@ def my_account(request):
             for notation in score_received:
                 dico = {}
                 if notation.score_pending:
-                    dico['score_status'] = 'pending'
-                    dico['role'] = 'receiver'
-                    dico['sender'] = User.objects.get(id=notation.sender_id).username
+                    dico["score_status"] = "pending"
+                    dico["role"] = "receiver"
+                    dico["sender"] = User.objects.get(id=notation.sender_id).username
                     scores_dict_list.append(dico)
                 else:
-                    dico['score_status'] = 'completed'
-                    dico['role'] = 'receiver'
-                    dico['sender'] = User.objects.get(id=notation.sender_id).username
-                    dico['score_sent'] = notation.score_sent
-                    dico['score_received'] = notation.score_received
+                    dico["score_status"] = "completed"
+                    dico["role"] = "receiver"
+                    dico["sender"] = User.objects.get(id=notation.sender_id).username
+                    dico["score_sent"] = notation.score_sent
+                    dico["score_received"] = notation.score_received
                     scores_dict_list.append(dico)
 
         if score_sent:
             for notation in score_sent:
                 dico = {}
                 if notation.score_pending:
-                    dico['score_status'] = 'pending'
-                    dico['role'] = 'sender'
-                    dico['receiver'] = User.objects.get(id=notation.receiver_id).username
+                    dico["score_status"] = "pending"
+                    dico["role"] = "sender"
+                    dico["receiver"] = User.objects.get(
+                        id=notation.receiver_id
+                    ).username
                     scores_dict_list.append(dico)
                 else:
-                    dico['score_status'] = 'completed'
-                    dico['role'] = 'sender'
-                    dico['receiver'] = User.objects.get(id=notation.receiver_id).username
-                    dico['score_sent'] = notation.score_sent
-                    dico['score_received'] = notation.score_received
+                    dico["score_status"] = "completed"
+                    dico["role"] = "sender"
+                    dico["receiver"] = User.objects.get(
+                        id=notation.receiver_id
+                    ).username
+                    dico["score_sent"] = notation.score_sent
+                    dico["score_received"] = notation.score_received
                     scores_dict_list.append(dico)
 
-        print(scores_dict_list)
+        context = {"choice": choice, "scores_dict_list": scores_dict_list}
 
-        context = {'choice':choice, 'scores_dict_list':scores_dict_list}
+        return render(request, "useraccount/myaccount.html", context)
 
-        return render(request, 'useraccount/myaccount.html', context)
+    return render(
+        request, "useraccount/myaccount.html", {"choice": "mon_profil", "form": form}
+    )
 
-    return render(request, "useraccount/myaccount.html", {'choice':'mon_profil', 'form':form})
 
 @login_required
 def score(request):
     """Manages score system between two registered members"""
     receiver_username = request.GET.get("receiver")
     receiver = User.objects.get(username=receiver_username)
-    if request.method == 'POST':
-        sender_rating_choice = request.POST.get('rating_value')
-        registration = ScoreRegistration(receiver_username, sender_rating_choice, request.user)
+    if request.method == "POST":
+        sender_rating_choice = request.POST.get("rating_value")
+        registration = ScoreRegistration(
+            receiver_username, sender_rating_choice, request.user
+        )
         registration.score_status_verification()
-        if registration.score_status == "Vous avez déjà noté cette personne, elle doit maintenant vous noter en retour":
-            messages.info(request, ('Vous avez déjà noté cette personne, elle doit maintenant vous noter en retour'))
-        if registration.score_status == 'Vous avez noté cette personne, elle a répondu, notation complète':
-            messages.info(request, ('Vous avez noté cette personne, elle a répondu, notation complète'))
-        if registration.score_status == "Cette personne vous a déjà noté, nous enregistrons votre note envers elle":
-            messages.info(request, ('Cette personne vous a déjà noté, nous enregistrons votre note envers elle'))
+        if (
+            registration.score_status
+            == "Vous avez déjà noté cette personne, elle doit maintenant vous noter en retour"
+        ):
+            messages.info(
+                request,
+                (
+                    "Vous avez déjà noté cette personne, elle doit maintenant vous noter en retour"
+                ),
+            )
+        if (
+            registration.score_status
+            == "Vous avez noté cette personne, elle a répondu, notation complète"
+        ):
+            messages.info(
+                request,
+                ("Vous avez noté cette personne, elle a répondu, notation complète"),
+            )
+        if (
+            registration.score_status
+            == "Cette personne vous a déjà noté, nous enregistrons votre note envers elle"
+        ):
+            messages.info(
+                request,
+                (
+                    "Cette personne vous a déjà noté, nous enregistrons votre note envers elle"
+                ),
+            )
             note = Rating.objects.filter(sender_id=receiver)
             note = note[0]
             note.score_received = int(sender_rating_choice)
             note.score_pending = False
             note.save()
-        if registration.score_status =='Cette personne vous a noté, vous avez déjà répondu, notation complète':
-            messages.info(request, ('Cette personne vous a noté, vous avez déjà répondu, notation complète'))
-        if registration.score_status == 'Vous êtes le premier à noter cette personne, nous enregistrons votre note':
-            messages.info(request, ('Vous êtes le premier à noter cette personne, nous enregistrons votre note et la prévenons par mail'))
-            note = Rating(sender_id=request.user.pk, receiver_id=(User.objects.get(username=receiver_username)).pk, score_sent=int(sender_rating_choice))
+        if (
+            registration.score_status
+            == "Cette personne vous a noté, vous avez déjà répondu, notation complète"
+        ):
+            messages.info(
+                request,
+                (
+                    "Cette personne vous a noté, vous avez déjà répondu, notation complète"
+                ),
+            )
+        if (
+            registration.score_status
+            == "Vous êtes le premier à noter cette personne, nous enregistrons votre note"
+        ):
+            messages.info(
+                request,
+                (
+                    "Vous êtes le premier à noter cette personne, nous enregistrons votre note et la prévenons par mail"
+                ),
+            )
+            note = Rating(
+                sender_id=request.user.pk,
+                receiver_id=(User.objects.get(username=receiver_username)).pk,
+                score_sent=int(sender_rating_choice),
+            )
             note.save()
             send_mail(
                 "Bonjour {}, c'est PeopleSkills".format(receiver.username),
-                "L'utilisateur {} souhaite vous attribuer une note, notez-le en retour.\n Rendez-vous sur votre page 'mon compte', onglet 'mes avis'".format(request.user.username),
+                "L'utilisateur {} souhaite vous attribuer une note, notez-le en retour.\nRendez-vous sur votre page 'mon compte', onglet 'mes avis'".format(
+                    request.user.username
+                ),
                 "vincent.nowak@hotmail.fr",
                 [receiver.email],
             )
 
-
-    return render(request, "useraccount/score.html", {'receiver':receiver_username})
+    return render(request, "useraccount/score.html", {"receiver": receiver_username})
