a06c03e6ac0e17c543cf2f49e36a33a83db126c7
Vincent74230 <vincent.nowak@hotmail.fr>
2021-07-06 19:16:22 +0200

    MODIFIED my smtp server from outlook to gmail, to see if it works

--- peopleskills/settings.py
+++ peopleskills/settings.py
@@ -168,9 +168,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-EMAIL_HOST = 'smtp-mail.outlook.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vincent.nowak@hotmail.fr'
+EMAIL_HOST_USER = 'vince74000@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
